64b4d6a3e818cd4e7bd819829b38a6cd94fb4a36
veriqus <android@veriqus.com>
2019-01-26 11:18:07 +0100

    Add basic soundpicker popup

--- app/src/main/java/com/example/deadmanswitch/MainFragment.kt
+++ app/src/main/java/com/example/deadmanswitch/MainFragment.kt
@@ -1,11 +1,14 @@
 package com.example.deadmanswitch
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.transaction
+import kotlinx.android.synthetic.main.card_tone_picker.*
 import kotlinx.android.synthetic.main.fragment_main.*
 import org.jetbrains.anko.startActivity
 
@@ -26,9 +29,28 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        premiumBar.setOnClickListener {
+        premiumBarMain.setOnClickListener {
             activity!!.startActivity<BuyPremiumActivity>()
         }
+        chooseToneButton.setOnClickListener {
+            openSoundPicker(view)
+        }
+    }
+
+    private fun openSoundPicker(view: View) {
+        val builder: AlertDialog.Builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        AlertDialog.Builder(view.context)
+        } else {
+            AlertDialog.Builder(view.context)
+        }
+
+        builder.setTitle("Credits")
+            .setMessage("Pick favorite")
+            .setPositiveButton(android.R.string.yes) { _, _ -> }
+            .setNegativeButton(android.R.string.no) { _, _ -> }
+            .setIcon(R.drawable.ic_check_green_24dp)
+            .setView(R.layout.single_row_feature) // you could specify your inner layout
+            .show()
     }
 
     fun newFrag() {
