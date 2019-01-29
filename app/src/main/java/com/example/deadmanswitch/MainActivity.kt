fbe5d4795640b1ab0a0135ed0b7306bf35fb347b
veriqus <android@veriqus.com>
2019-01-29 21:26:24 +0100

    Add basic notification

--- app/src/main/java/com/example/deadmanswitch/MainActivity.kt
+++ app/src/main/java/com/example/deadmanswitch/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.deadmanswitch
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.transaction
@@ -12,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : CustomStatusBarActivity(), SensorEventListener {
     private lateinit var mSensorManager: SensorManager
+    private var notificationManager: NotificationManager? = null
     private var mProximity: Sensor? = null
 
 
@@ -25,6 +33,9 @@ class MainActivity : CustomStatusBarActivity(), SensorEventListener {
         }
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notification("Title", "description")
     }
 
     override fun onResume() {
@@ -47,4 +58,69 @@ class MainActivity : CustomStatusBarActivity(), SensorEventListener {
         }
 
     override fun onAccuracyChanged(event: Sensor?,value: Int) {}
+
+    private fun notification(title: String, body: String) {
+
+        val resultIntent = Intent(this, MainActivity::class.java)
+
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        resultIntent.action = Intent.ACTION_MAIN
+        resultIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        resultIntent.putExtra("off", "TURN OFF")
+
+        val resultPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val mNotificationId = 1
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(
+                "com.example.deadmanswitch",
+                "Dead Man Switch",
+                "DMS Channel")
+            val mBuilder = Notification.Builder(this, "com.example.deadmanswitch")
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setContentIntent(resultPendingIntent)
+                .setSmallIcon(R.drawable.ic_moon)
+            notificationManager?.notify(1, mBuilder.build())
+
+        } else {
+            val mBuilder = Notification.Builder(this).apply {
+                setContentTitle(title)
+                setContentText(body)
+                setSmallIcon(R.drawable.ic_moon)
+                setContentIntent(resultPendingIntent)
+            }
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+            notificationManager?.notify(mNotificationId, mBuilder.build())
+
+        }
+
+    }
+
+    private fun createNotificationChannel(id: String, name: String,
+                                          description: String) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        } else {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel(id, name, importance)
+            } else {
+                TODO("VERSION.SDK_INT < O")
+            }
+            channel.description = description
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.vibrationPattern =
+                longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
 }
