8b1fc35f8782bb928758e9307c138ded587708b9
Krzysztof Marczewski <android@veriqus.com>
2019-03-04 17:12:51 +0100

    fix widget styling

--- app/src/main/java/com/example/deadmanswitch/RunAlarmWidget.kt
+++ app/src/main/java/com/example/deadmanswitch/RunAlarmWidget.kt
@@ -31,11 +31,8 @@ class RunAlarmWidget : AppWidgetProvider() {
             context: Context, appWidgetManager: AppWidgetManager,
             appWidgetId: Int
         ) {
-
-            val widgetText = context.getString(R.string.appwidget_text)
             // Construct the RemoteViews object
             val views = RemoteViews(context.packageName, R.layout.run_alarm_widget)
-            views.setTextViewText(R.id.appwidget_text, widgetText)
 
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
